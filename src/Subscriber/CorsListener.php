ccb31172c2c53b8dd0a43de9c1233ea4b7c0f1d1
marcelloroehrssen <marcello.roehrssen@gmail.com>
2020-04-30 18:27:31 +0200

    added cors listener

---
+++ src/Subscriber/CorsListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class CorsListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST  => array('onKernelRequest', 9999),
+            KernelEvents::RESPONSE => array('onKernelResponse', 9999),
+        );
+    }
+    public function onKernelRequest(RequestEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $method  = $event->getRequest()->getRealMethod();
+        if ('OPTIONS' == $method) {
+            $response = new Response();
+            $event->setResponse($response);
+        }
+    }
+
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'X-Header-One,X-Header-Two,client-security-token');
+    }
+
+}
